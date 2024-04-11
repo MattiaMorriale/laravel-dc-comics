4956ba4c2a727e57acfc730d8808c5230e6c0bd3
mattia_morriale <mattia.morriale1@gmail.com>
2024-04-11 18:49:10 +0200

    start the web site

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1 class="py-5">I miei comics</h1>
+    </div> 
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme='dark'>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Titolo</title>
+    <title>laravel-dc-comics</title>
 
     @vite('resources/js/app.js')
     
 </head>
 <body>
     
+    @yield('content')
+
 </body>
 </html>
