73bf9ed7bac6382b5538813ef9c42b0c87154a70
mattia_morriale <mattia.morriale1@gmail.com>
2024-04-17 12:16:26 +0200

    create a route and PageController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\ComicController;
+use App\Http\Controllers\Guest\PageController;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('comics', ComicController::class);
+
+Route::get('/', [PageController::class, 'home'])->name('home');
